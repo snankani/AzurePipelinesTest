8216ab26d11754e0788d897bbc54ef27e5bb5eaa
Satish Nankani <snankani@microsoft.com>
2020-08-14 16:10:09 +0530

    commit notification test

--- examples/calculateQueryScheduling.ps1
+++ examples/calculateQueryScheduling.ps1
@@ -1,4 +1,6 @@
-﻿#---------------------------------------------------------------------------------------------
+﻿# ignore the changes in this line. test in progress.
+
+#---------------------------------------------------------------------------------------------
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
