f43da53d65a47a1dc4ec51d4c4bad829749cd51e
Satish Nankani <snankani@microsoft.com>
2020-08-14 16:11:31 +0530

    commit notification test

--- examples/gettingStarted.ps1
+++ examples/gettingStarted.ps1
@@ -1,4 +1,6 @@
-﻿#---------------------------------------------------------------------------------------------
+﻿# ignore the changes in this line. test in progress.
+
+#---------------------------------------------------------------------------------------------
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
@@ -10,21 +12,18 @@ param(
     [String]$repoToImport
 )
 
-if([string]::IsNullOrWhiteSpace($org))
-{
+if ([string]::IsNullOrWhiteSpace($org)) {
     $org = Read-Host 'What is your organization? (ex: https://contoso.visualstudio.com/)'
 }
 
-if([string]::IsNullOrWhiteSpace($projectName))
-{
+if ([string]::IsNullOrWhiteSpace($projectName)) {
     $projectName = Read-Host 'Please enter name of project to be used for bugbash'
 }
 
 $project = az devops project create --org $org --name $projectName -o json | ConvertFrom-Json
 Write-Host "Created new project with Id $($project.id)" 
 
-if([string]::IsNullOrWhiteSpace($repoName))
-{
+if ([string]::IsNullOrWhiteSpace($repoName)) {
     $repoName = Read-Host "Please enter name of repo to be used for bugbash"
 }
 
@@ -32,8 +31,7 @@ $repo = az repos create --org $org -p $project.id --name $repoName -o json | Con
 Write-Host "Create repo with Id $($repo.id)"
 
 
-if([string]::IsNullOrWhiteSpace($repoToImport))
-{
+if ([string]::IsNullOrWhiteSpace($repoToImport)) {
     $repoToImport = Read-Host "Please give source of repo to import"
 }
 
